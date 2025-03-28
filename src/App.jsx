1ddba707a9fa071f0a39db11b495d86e808aa2e0
Subu <Subramanyammolleti@gmail.com>
2025-03-27 22:49:44 -0400

    Context example with theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
-import './App.css'
+import { useState, useEffect } from 'react';
+
+import './App.css';
+import Card from './components/Card';
+import ThemeBtn from './components/ThemeBtn';
+import { ThemeProvider } from './context/Theme';
 
 function App() {
+  const [themeMode, setThemeMode] = useState('light');
+
+  const darkTheme = () => {
+    setThemeMode('dark');
+  };
+
+  const lightTheme = () => {
+    setThemeMode('light');
+  };
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove('dark', 'light');
+    document.querySelector('html').classList.toggle(themeMode);
+    // TODO improve the logic for context
+    // document.querySelector('html').classList.toggle('dark');
+  }, [themeMode]);
 
   return (
-    <>
-      <h1 className="font-mono">Hello</h1>
-    </>
-  )
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+      <div className="flex justify-center items-center h-screen">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
